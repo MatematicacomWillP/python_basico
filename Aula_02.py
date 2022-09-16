6baa27d3e3c0239883dd14aa83857a2db3703955
William Pessoa <113470103+MatematicacomWillP@users.noreply.github.com>
2022-09-16 17:11:10 -0300

    Update Aula_02.py

--- Aula_02.py
+++ Aula_02.py
@@ -8,23 +8,8 @@
 # > maior   == igual       >= maior ou igual
 # < menor   != diferente   <= menor ou igual
 
-#== Exemplos ==#
-
-#1) Faça um programa que receba dois números e 
-#   mostre qual deles é o maior, e se caso for iguais, 
-#   mostrar a mensagem dizendo que são iguais.
-
-#valor1 = float(input("Digite o valor 1: "))
-#valor2 = float(input("Digite o valor 2: "))
-#
-#if valor1 > valor2:
-#    print('Valor 1: {} é maior que o Valor 2: {}'.format(valor1,valor2))
-#elif valor2 > valor1:
-#    print('Valor 2: {} é maior que o Valor 1: {}'.format(valor2,valor1))
-#else:
-#    print('Valor 1: {} é igual ao Valor 2: {}'.format(valor1,valor2))
-
-#2) A nota final de um estudante é calculada a partir de 
+#==> Exemplo 1 --> Condicionais
+#   A nota final de um estudante é calculada a partir de 
 #   três notas atribuídas entre o intervalo de 0 até 10, 
 #   respectivamente, nota 1, nota 2
 #   e a nota 3. A média das três notas mencionadas 
