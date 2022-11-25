0004cd35a34cd3f5d05a8e768f0cdd99cf2cb3dc
William Pessoa <113470103+MatematicacomWillP@users.noreply.github.com>
2022-11-25 17:21:48 -0300

    Add files via upload

--- estatistica_parte3_1.py
+++ estatistica_parte3_1.py
@@ -3,6 +3,7 @@
 import math
 from pandas import*
 
+
 #== Entrada de dados ==#
 
 dados = []
@@ -44,20 +45,43 @@ print('A amplitude do intervalo: ',h)
 
 classe = []
 bins = []
+c = [] #Média do intervalo de classe
 menor = min1
 max_amp = menor + h
 valor = menor
 bins.append(valor)
 while valor < max1:
-    classe.append('{}|--{}'.format(valor,valor + h))
+    classe.append('{} |-- {}'.format(valor,valor + h))
+    media = (valor + (valor+h))/2
     valor += h
     bins.append(valor)
+    c.append(media)
 
 #== Contando as Frequências ==#
+
 for i in dados:
     ultimo = i
+
 if ultimo == max1:
-    fr = cut(dados, bins=k, labels=classe, right=False, include_lowest=False).value_counts()
+    fi = cut(dados, bins=k, right=False, include_lowest=False).value_counts().to_list()
 else:
-    fr = cut(dados, bins, labels=classe, right=False, include_lowest=False).value_counts()
-print(fr)
+    fi = cut(dados, bins, right=False, include_lowest=False).value_counts().to_list()
+print(fi)
+
+Fi = [] #Frequência absoluta acumulada
+fr = [] #Frequência relativa
+Fri = [] #Frequência relativa acumulada
+
+soma1 = 0 #Fi
+soma2 = 0 #Fri
+for i in fi:
+    soma1 += i
+    Fi.append(soma1)
+    div = i/n
+    fr.append('{:.3f}'.format(div))
+    soma2 += div
+    Fri.append('{:.3f}'.format(soma2))
+    
+df = DataFrame({'Classe':classe, 'fi':fi, 'Fi':Fi, 'fr':fr, 'Fr':Fri, 'c':c})
+df = df.to_string(index = False)
+print(df)
